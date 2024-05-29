2fbd6172d0921f6aa49bec789299bc66ab7302b4
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-05-29 08:33:01 -0300

    Add files via upload

---
+++ alternar.js
@@ -0,0 +1,14 @@
+const Pessoa ={
+    nome:"Luma",
+    profissao:"Engenharia",
+};
+
+console.log(Pessoa.telefone);
+
+Pessoa.telefone = "41 9999-9999";
+
+console.log(Pessoa.telefone);
+
+Pessoa.nome = "Luma Silva";
+
+console.log(Pessoa)
---
+++ tipos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email:"jao@yahoo.com",
+    telefone:["(41) 9999-0000","(42) 9988-0011"],
+};
+
+console.log(cliente)
+console.log(cliente.telefone)
